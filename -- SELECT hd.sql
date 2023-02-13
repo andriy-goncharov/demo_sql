8b301f58a5cfbbe900cffc8d1001fa16e75d89f7
parazite1 <andergoncharov@gmail.com>
2023-02-13 12:40:46 +0200

    Update -- SELECT hd.sql

--- -- SELECT hd.sql
+++ -- SELECT hd.sql
@@ -3,6 +3,11 @@
 -- GROUP BY hd 
 -- HAVING COUNT(*) >= 2
 
+-- SELECT speed, AVG(price) as price
+-- from PC
+-- WHERE speed >600
+-- GROUP by speed
+
 SELECT speed, AVG(price) as price
 from PC
 WHERE speed >600
