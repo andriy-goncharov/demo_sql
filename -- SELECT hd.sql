d158242325c6c21b7752e93567991d67c13ffc92
ander <andergoncharov@gmail.com>
2023-02-14 14:25:28 +0200

    next additions

--- -- SELECT hd.sql
+++ -- SELECT hd.sql
@@ -3,10 +3,18 @@
 -- GROUP BY hd 
 -- HAVING COUNT(*) >= 2
 
+
+-- SELECT speed, AVG(price) as price
+-- from PC 
+-- WHERE speed >600
+-- GROUP by speed
+
+
 -- SELECT speed, AVG(price) as price
 -- from PC
 -- WHERE speed >600
 -- GROUP by speed
+
 
 SELECT speed, AVG(price) as price
 from PC
