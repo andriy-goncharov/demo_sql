e9e4c7d2f099a4657114e1fb977712a7c102b9f6
ander <andergoncharov@gmail.com>
2023-02-14 20:39:07 +0200

    adding new enters

--- -- SELECT hd.sql
+++ -- SELECT hd.sql
@@ -2,17 +2,19 @@
 -- FROM PC
 -- GROUP BY hd 
 -- HAVING COUNT(*) >= 2
-
-
 -- SELECT speed, AVG(price) as price
 -- from PC 
+
 -- WHERE speed >600
+
 -- GROUP by speed
 
-
 -- SELECT speed, AVG(price) as price
+
 -- from PC
+
 -- WHERE speed >600
+
 -- GROUP by speed
 
 
@@ -20,4 +22,3 @@ SELECT speed, AVG(price) as price
 from PC
 WHERE speed >600
 GROUP by speed
-
